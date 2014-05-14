4294de4ce56e96fdc509bec1a8929d9d5e64e3f7
shahaf zor <shahaf_zor@hotmail.com>
2014-05-14 16:11:04 +0300

    Changed the name of the login file on the server from index.php to login.php

--- src/com/moti/tellatale/ServerUrls.java
+++ src/com/moti/tellatale/ServerUrls.java
@@ -7,6 +7,6 @@ public final class ServerUrls
 	public static final String SERVER_SEND_URL = "send_story.php";
 	public static final String SERVER_RECEIVE_URL = "receive_story.php";
 	public static final String SERVER_SEND_MY_STORIES_URL = "send_my_stories.php";
-	public static final String LOGIN_URL = "index.php";
+	public static final String LOGIN_URL = "login.php";
 	public static final String NEW_USER_URL = "add_user.php";
 }
